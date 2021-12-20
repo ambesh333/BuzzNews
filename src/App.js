a334b7ca7409e5172a4912be88b8bc7c2c3b98b6
Ambesh <ambeshgaunker@gmail.com>
2021-12-20 15:25:41 +0530

    adding router

--- src/App.js
+++ src/App.js
@@ -5,13 +5,40 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
+
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+   
+    
+  } from "react-router-dom";
+
+
 export default class App extends Component {
      
     render() {
         return (
             <div>
+                <Router>
+
                 <Navbar/>
-                <News pageSize={5} country="in" category="science"/>
+                <Routes >
+                         
+                <Route exact path="/" element={<News key="general" pageSize={5} country="in" category="general"/>}/>
+                <Route exact path="/business" element={<News key="business" pageSize={5} country="in" category="business"/>} />
+                <Route exact path="/Entertainment" element={<News key="Entertainment" pageSize={5} country="in" category="Entertainment"/>}/>
+                <Route exact path="/General" element={<News key="General" pageSize={5} country="in" category="General"/>}/>
+                <Route exact path="/Health" element={<News key="Health" pageSize={5} country="in" category="Health"/>}/>
+                <Route exact path="/Science" element={<News key="Science" pageSize={5} country="in" category="Science"/>}/>
+                <Route exact path="/Sports" element={<News  key="Sports"pageSize={5} country="in" category="Sports"/> }/>
+                <Route exact path="/Technology" element={<News key="Technology" pageSize={5} country="in" category="Technology"/>}/>
+                
+            
+               
+                 key=""
+                         </Routes>
+                </Router>
             </div>
         )
     }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+import {
+  
+  Link,
+ 
+} from "react-router-dom";
+
 export class Navbar extends Component {
 
 
@@ -8,39 +14,40 @@ export class Navbar extends Component {
             <div>
               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">BuzzNews</a>
+    <Link className="navbar-brand" to="/">BuzzNews</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0"  style={{ padding:"2px"}}>
+        
+        <li className="nav-item" style={{ padding:"12px 45px"}} >
+          <Link className="nav-Link active" aria-current="page" to="/" style={{ textDecoration: 'none' , color: '#FFF'}}>Home</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/About">About</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/About" style={{ textDecoration: 'none', color: '#FFF'}}>About</Link>
         </li>
         
-        <li className="nav-item">
-          <a className="nav-link" href="/Business">Business</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/Business" style={{ textDecoration: 'none', color: '#FFF' }} >Business</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Entertainment">Entertainment</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/Entertainment" style={{ textDecoration: 'none', color: '#FFF' }} s>Entertainment</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/General">General</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/General" style={{ textDecoration: 'none' , color: '#FFF'}} >General</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Health">Health</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/Health" style={{ textDecoration: 'none', color: '#FFF' }} >Health</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Science">Science</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/Science" style={{ textDecoration: 'none', color: '#FFF' }} >Science</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Sports">Sports</a>
+        <li className="nav-item" style={{ padding:"12px 45px"}}>
+          <Link className="nav-Link" to="/Sports" style={{ textDecoration: 'none', color: '#FFF' }} >Sports</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/Technology">Technology</a>
+        <li className="nav-item" style={{ padding:"12px  45px"}}>
+          <Link className="nav-Link" to="/Technology" style={{ textDecoration: 'none', color: '#FFF' }} >Technology</Link>
         </li>
         
     
